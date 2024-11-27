8da716ae9e9703030b23cc40feb52cba321f62d8
Kritika Kanchan <kritika.kanchan_cs.h24@gla.ac.in>
2024-11-27 23:35:55 +0530

    function_q02.py

---
+++ coding-assignment/function/function_q02.py
@@ -0,0 +1,5 @@
+# Define a function named 'string_reverse' that takes a string 'str1' as input and reverse the given string
+def string_reverse(str1):
+    rstr1 = str1[::-1]
+    return rstr1
+print(string_reverse('1234abcd'))
