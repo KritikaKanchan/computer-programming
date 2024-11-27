341e05bcedb52a55d4c382a70cc0dc91477dcaea
Kritika Kanchan <kritika.kanchan_cs.h24@gla.ac.in>
2024-11-28 00:04:57 +0530

    string_q01.py

---
+++ coding-assignment/string/string_q01.py
@@ -0,0 +1,5 @@
+#write a python program where user will enter the name and value of n(int) now task is you have to display your name and times
+name=input("enter name")
+n=int(input("enter how many times you want to repeat"))
+for i in range(n):
+    print(name)
