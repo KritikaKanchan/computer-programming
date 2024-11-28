93d973183112539cd8b44b9966191e3334134a5a
Kritika Kanchan <kritika.kanchan_cs.h24@gla.ac.in>
2024-11-28 11:18:25 +0530

    function_q16.py

---
+++ coding-assignment/function/function_q16.py
@@ -0,0 +1,6 @@
+#Write a function that calculates the area of a circle given its radius.
+import math
+
+def area_of_circle(radius):
+    return math.pi * radius * radius
+print(area_of_circle(5))  # Output: 78.53981633974483
