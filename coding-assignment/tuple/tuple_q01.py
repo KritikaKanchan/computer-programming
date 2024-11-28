2b7a806df6455eeb8f7aa0ebc7094bd58f829842
Kritika Kanchan <kritika.kanchan_cs.h24@gla.ac.in>
2024-11-28 08:52:23 +0530

    tuple_q01.py

---
+++ coding-assignment/tuple/tuple_q01.py
@@ -0,0 +1,6 @@
+#Problem: Find the Maximum and Minimum Values in a Tuple
+def find_max_min(tpl):
+    return (max(tpl), min(tpl))
+
+tpl = (5, 12, 3, 9, 7)
+print(find_max_min(tpl))  # Output: (12, 3)
