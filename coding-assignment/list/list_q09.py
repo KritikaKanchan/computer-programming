9b0ae4590e347f3a971e3574dd30518b4187fbdc
Kritika Kanchan <kritika.kanchan_cs.h24@gla.ac.in>
2024-11-28 14:33:55 +0530

    list_q09.py

---
+++ coding-assignment/list/list_q09.py
@@ -0,0 +1,9 @@
+# Write a Python function that takes a nested list and flattens it into a single list.
+def flatten(lst):
+    flat_list = []
+    for item in lst:
+        if isinstance(item, list):
+            flat_list.extend(flatten(item))
+        else:
+            flat_list.append(item)
+    return flat_list
