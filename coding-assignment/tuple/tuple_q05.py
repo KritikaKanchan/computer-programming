c896e2a7c41c0cc060197f7b827ba983e110a544
Kritika Kanchan <kritika.kanchan_cs.h24@gla.ac.in>
2024-11-28 08:59:26 +0530

    tuple_q05.py

---
+++ coding-assignment/tuple/tuple_q05.py
@@ -0,0 +1,8 @@
+#Problem: Concatenate Two Tuples
+def concatenate_tuples(tpl1, tpl2):
+    return tpl1 + tpl2
+
+# Test case
+tpl1 = (1, 2, 3)
+tpl2 = (4, 5, 6)
+print(concatenate_tuples(tpl1, tpl2))  # Output: (1, 2, 3, 4, 5, 6)
